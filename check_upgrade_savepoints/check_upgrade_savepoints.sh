3d77c65a606166b1f6d2796284777bb85e700ec8
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2012-05-24 02:53:35 +0200

    Support $phpcmd & strict execution

--- check_upgrade_savepoints/check_upgrade_savepoints.sh
+++ check_upgrade_savepoints/check_upgrade_savepoints.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# $phpcmd: Path to the PHP CLI executable
 # $gitdir: Directory containing git repo
 # $gitbranch: Branch we are going to check
+
+# Let's go strict (exit on error)
+set -e
 
 # file where results will be sent
 resultfile=$WORKSPACE/check_upgrade_savepoints_${gitbranch}.txt
@@ -16,7 +20,7 @@ cd $gitdir && git checkout $gitbranch && git reset --hard origin/$gitbranch
 cp $mydir/check_upgrade_savepoints.php $gitdir/
 
 # Run the savpoints checker utility, saving results to file
-/opt/local/bin/php $gitdir/check_upgrade_savepoints.php > "$resultfile"
+${phpcmd} $gitdir/check_upgrade_savepoints.php > "$resultfile"
 
 # remove the checker from gitdir
 rm -fr $gitdir/check_upgrade_savepoints.php
