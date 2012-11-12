85ed9d85098cb98c3b5ca47f64d1114c59ab171d
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2012-11-12 12:34:34 +0100

    HQ-325 ci: Moving to own repository. Minor bits.

---
+++ README.md
@@ -0,0 +1,20 @@
+# CI local plugin
+
+This local_ci plugin contains all the scripts needed
+by Moodle CI servers to automate checks while
+integration happens.
+
+## Dependencies
+
++ Some checks require a MySQL, master-based site to be up and running.
++ Some checks require the installation of 3rd part tools (phpunit...).
++ Some checks require the presence of both local_codechecker and local_moodlecheck local plugins.
+
+## TODO
+
++ Complete the documentation.
++ Document each check properly.
+
+## Self-versions
+
++ 20121112 - Eloy - Initial version
---
+++ version.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * CI scripts version information
+ *
+ * @package    local_ci
+ * @copyright  2012 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2012111200;
+$plugin->requires  = 2012110900;        // Moodle 2.4beta release and upwards.
+$plugin->dependencies = array(          // Also requires these plugins to be installed.
+    'local_codechecker' => '2012061600',
+    'local_moodlecheck' => '2012011000'
+);
+$plugin->component = 'local_ci';
+$plugin->release   = '0.9.0';
+$plugin->maturity  = MATURITY_BETA;
