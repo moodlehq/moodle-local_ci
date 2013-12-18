ea270568f0759b3d4c791f4a9bced64e2ed451c8
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2013-12-18 19:44:33 +0100

    NOBUG: Skip security issues from the bulk checker.
    They don't contain branches to be inspected (patches are used),
    so no much to check automatically there.

--- tracker_automations/bulk_precheck_issues/criteria/awaiting_peer_review/query.sh
+++ tracker_automations/bulk_precheck_issues/criteria/awaiting_peer_review/query.sh
@@ -2,6 +2,7 @@ ${basereq} --action getIssueList \
            --search "project = 'Moodle' \
                  AND status = 'Waiting for peer review' \
                  AND (labels IS EMPTY OR labels NOT IN (ci, security_held, integration_held)) \
+                 AND level IS EMPTY \
                  ORDER BY priority DESC, votes DESC, 'Last comment date' ASC" \
            --outputFormat 101 \
            --file "${resultfile}"
