60d66019fa04809578f38324fd358393049e72a4
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2013-12-23 16:51:44 +0100

    NOBUG: Restrict comments to integrators.
    While we advance a bit on prechecker improvements, when
    non-quiet operations are selected for awaiting integration
    issues, we restrict comments to integrators. Results will help to:
    
    1) integrate the issue.
    2) discover things to be added to the prechecker.

--- tracker_automations/bulk_precheck_issues/criteria/awaiting_integration/postissue.sh
+++ tracker_automations/bulk_precheck_issues/criteria/awaiting_integration/postissue.sh
@@ -3,8 +3,10 @@ ${basereq} --action addLabels \
     --issue ${issue} \
     --labels "ci"
 
-# Add the comment with results
+# Add the comment with results.
+# (Eloy 20131223 - restricted to Integrators)
 comment=$(cat "${resultfile}.${issue}.txt")
 ${basereq} --action addComment \
     --issue ${issue} \
-    --comment "${comment}"
+    --comment "${comment}" \
+    --role "Integrators"
