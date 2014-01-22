533a6012e04799506c0e5bccc2b6bd8bcc4f59a5
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2014-01-22 23:53:29 +0100

    MDLSITE-2714 All criteria to clean the cime label
    Before this, it was possible for the chain of precheckers
    to pick the same issue more than once, if the "cime" label
    was added to an issue sent to peer review or integration.
    
    With this change, he "cime" label is always cleaned by all
    criteria so no dupe executions are possible anymore.

--- tracker_automations/bulk_precheck_issues/criteria/awaiting_integration/postissue.sh
+++ tracker_automations/bulk_precheck_issues/criteria/awaiting_integration/postissue.sh
@@ -3,6 +3,11 @@ ${basereq} --action addLabels \
     --issue ${issue} \
     --labels "ci"
 
+# Remove the "cime" label.
+${basereq} --action removeLabels \
+    --issue ${issue} \
+    --labels "cime"
+
 # Add the comment with results.
 # (Eloy 20131223 - restricted to Integrators)
 comment=$(cat "${resultfile}.${issue}.txt")
--- tracker_automations/bulk_precheck_issues/criteria/awaiting_peer_review/postissue.sh
+++ tracker_automations/bulk_precheck_issues/criteria/awaiting_peer_review/postissue.sh
@@ -3,6 +3,11 @@ ${basereq} --action addLabels \
     --issue ${issue} \
     --labels "ci"
 
+# Remove the "cime" label.
+${basereq} --action removeLabels \
+    --issue ${issue} \
+    --labels "cime"
+
 # Add the comment with results
 comment=$(cat "${resultfile}.${issue}.txt")
 ${basereq} --action addComment \
