4c52b459aff7f1b113590b5e11a7a6d20a10cef5
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2016-06-15 15:58:14 +0200

    MDL-52127 eslint: ignorefiles task is not there any more
    Just decided to comment it instead of deleting in case there
    is a chance of getting it back when the subdirectories problem
    is fixed and the task is back. See MDL-54930 for more info.

--- remote_branch_checker/remote_branch_checker.sh
+++ remote_branch_checker/remote_branch_checker.sh
@@ -372,7 +372,7 @@ find ${WORKSPACE} -type d -depth -empty -and -not \( -name .git -or -name work \
 if [ -f $WORKSPACE/.eslintrc ]; then
     eslintcmd="$(${npmcmd} bin)"/eslint
     if [ -x $eslintcmd ]; then
-        "$(${npmcmd} bin)"/grunt ignorefiles
+        #"$(${npmcmd} bin)"/grunt ignorefiles
         $eslintcmd -f checkstyle $WORKSPACE > "${WORKSPACE}/work/eslint.xml"
     else
         echo "Error: .eslintrc file found, but eslint executable not found" | tee -a ${errorfile}
