192b956c1f6bca2e127542ffb2f29a605520eff0
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2016-08-26 02:59:48 +0200

    MDLSITE-4722 various improvs to the compare db shell script
    List of changes:
    
    - Improved handling of arguments.
    - Separate 100% from moodle_ci_site (uses gitdir instead).
    - Better git operations preventing dettached heads.
    - Added support to check upgrade from multiple origins, keeping BC.
    - Support executions without Jenkins.
    - Complete output sent to logfile.
    - Reduced output includes results.

--- compare_databases/compare_databases.sh
+++ compare_databases/compare_databases.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+# $gitcmd: Path to the git executable
 # $phpcmd: Path to the PHP CLI executable
 # $mysqlcmd: Path to the mysql CLI executable
 # $gitdir: Directory containing git repo
 # $gitbranchinstalled: Branch we are going to install the DB (and upgrade to)
-# $gitbranchupgraded: Branch we are going to upgrade the DB from
+# $gitbranchupgraded: Branch we are going to upgrade the DB from (supports multiple, separated by commas)
 # $dblibrary: Type of library (native, pdo...)
 # $dbtype: Name of the driver (mysqli...)
 # $dbhost1: DB1 host
@@ -16,104 +17,147 @@
 # Don't be strict. Script has own error control handle
 set +e
 
-# file where results will be sent
-resultfile=$WORKSPACE/compare_databases_${gitbranchinstalled}_${gitbranchupgraded}.txt
+# Verify everything is set
+required="WORKSPACE gitcmd phpcmd mysqlcmd gitdir gitbranchinstalled gitbranchupgraded dblibrary dbtype dbhost1 dbuser1 dbpass1"
+for var in ${required}; do
+    if [ -z "${!var}" ]; then
+        echo "Error: ${var} environment variable is not defined. See the script comments."
+        exit 1
+    fi
+done
+
+# Convert $gitbranchupgraded to array, so we can iterate later, keeping BC
+upgradedarr=(${gitbranchupgraded//,/ })
+echo "Info: Origin branches: (${#upgradedarr[@]}) $gitbranchupgraded"
+echo "Info: Target branch: $gitbranchinstalled"
+echo
 
 # calculate some variables
+BUILD_NUMBER="${BUILD_NUMBER:-$PPID}"
+EXECUTOR_NUMBER="${EXECUTOR_NUMBER:-0}"
+dbprefixinstall="cii_"
+dbprefixupgrade="ciu_"
+dbhost2="${dbhost2:-$dbhost1}"
+dbuser2="${dbuser2:-$dbuser1}"
+dbpass2="${dbpass2:-$dbpass1}"
 mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+currentbranch=$( cd $gitdir && $gitcmd rev-parse -q --abbrev-ref HEAD )
 installdb=ci_installed_${BUILD_NUMBER}_${EXECUTOR_NUMBER}
 upgradedb=ci_upgraded_${BUILD_NUMBER}_${EXECUTOR_NUMBER}
 datadir=/tmp/ci_dataroot_${BUILD_NUMBER}_${EXECUTOR_NUMBER}
-dbprefixinstall="cii_"
-dbprefixupgrade="ciu_"
-if [[ -z "$dbhost2" ]]
-then
-    dbhost2=$dbhost1
-fi
-if [[ -z "$dbuser2" ]]
-then
-    dbuser2=$dbuser1
-fi
-if [[ -z "$dbpass2" ]]
-then
-    dbpass2=$dbpass1
-fi
+logfile=$WORKSPACE/compare_databases_${gitbranchinstalled}_logfile.txt
+touch "${logfile}"
 
 # Going to install the $gitbranchinstalled database
 # Create the database to install
-# TODO: Based on $dbtype, execute different DB creation commands
-${mysqlcmd} --user=$dbuser1 --password=$dbpass1 --host=$dbhost1 --execute="CREATE DATABASE $installdb CHARACTER SET utf8 COLLATE utf8_bin"
+#  TODO: Based on $dbtype, execute different DB creation commands
+echo "Info: Creating $installdb database" | tee -a "${logfile}"
+${mysqlcmd} --user=$dbuser1 --password=$dbpass1 --host=$dbhost1 \
+    --execute="CREATE DATABASE $installdb CHARACTER SET utf8 COLLATE utf8_bin" 2>&1 >> "${logfile}"
 # Error creating DB, we cannot continue. Exit
 exitstatus=${PIPESTATUS[0]}
 if [ $exitstatus -ne 0 ]; then
-    echo "Error creating database $installdb to test upgrade"
-    exit $exitstatus
-fi
-
-# Create the database to upgrade
-# TODO: Based on $dbtype, execute different DB creation commands
-${mysqlcmd} --user=$dbuser1 --password=$dbpass1 --host=$dbhost1 --execute="CREATE DATABASE $upgradedb CHARACTER SET utf8 COLLATE utf8_bin"
-# Error creating DB, we cannot continue. Exit
-exitstatus=${PIPESTATUS[0]}
-if [ $exitstatus -ne 0 ]; then
-    echo "Error creating database $upgradedb to test upgrade"
+    echo "Error: Problem creating database $installdb" | tee -a "${logfile}"
     exit $exitstatus
 fi
 
 # Do the moodle install of $installdb
-cd $gitdir && git reset --hard $gitbranchinstalled
+echo "Info: Installing Moodle $gitbranchinstalled into $installdb" | tee -a "${logfile}"
+cd $gitdir && $gitcmd checkout -q -B installbranch $gitbranchinstalled
 rm -fr config.php
-${phpcmd} admin/cli/install.php --non-interactive --allow-unstable --agree-license --wwwroot="http://localhost" --dataroot="$datadir" --dbtype=$dbtype --dbhost=$dbhost1 --dbname=$installdb --dbuser=$dbuser1 --dbpass=$dbpass1 --prefix=$dbprefixinstall --fullname=$installdb --shortname=$installdb --adminuser=$dbuser1 --adminpass=$dbpass1
+${phpcmd} admin/cli/install.php --non-interactive --allow-unstable --agree-license --wwwroot="http://localhost" --dataroot="$datadir" --dbtype=$dbtype --dbhost=$dbhost1 --dbname=$installdb --dbuser=$dbuser1 --dbpass=$dbpass1 --prefix=$dbprefixinstall --fullname=$installdb --shortname=$installdb --adminuser=$dbuser1 --adminpass=$dbpass1 2>&1 >> "${logfile}"
 # Error installing, we cannot continue. Exit
 exitstatus=${PIPESTATUS[0]}
 if [ $exitstatus -ne 0 ]; then
-    echo "Error installing $gitbranchinstalled to test upgrade"
+    echo "Error: Problem installing Moodle $gitbranchinstalled" | tee -a "${logfile}"
 fi
 
-# Going to install and upgrade the $gitbranchupgraded database
+# Count problematic exitstatuses
+counterrors=0
 
-# Do the moodle install of $upgradedb
-# only if we don't come from an erroneus previous situation
-if [ $exitstatus -eq 0 ]; then
-    # Detached head, good enough
-    cd $gitdir && git checkout origin/$gitbranchupgraded
+# Iterate over the $upgradedarr (list of comma separated $gitbranchupgraded)
+for upgrade in "${upgradedarr[@]}"; do
+
+    echo | tee -a "${logfile}"
+    echo "Info: Comparing $gitbranchinstalled and upgraded $upgrade" | tee -a "${logfile}"
+
+    # Create the database to upgrade
+    # TODO: Based on $dbtype, execute different DB creation commands
+    echo "Info: Creating $upgradedb database" | tee -a "${logfile}"
+    ${mysqlcmd} --user=$dbuser2 --password=$dbpass2 --host=$dbhost2 \
+        --execute="CREATE DATABASE $upgradedb CHARACTER SET utf8 COLLATE utf8_bin" 2>&1 >> "${logfile}"
+    # Error creating DB, we cannot continue. Exit
+    exitstatus=${PIPESTATUS[0]}
+    if [ $exitstatus -ne 0 ]; then
+        echo "Error: Problem creating database $upgradedb to test upgrade" | tee -a "${logfile}"
+    fi
+
+    # Do the moodle install of $upgradedb
+    # only if we don't come from an erroneus previous situation
+    if [ $exitstatus -eq 0 ]; then
+        echo "Info: Installing Moodle $upgrade into $upgradedb" | tee -a "${logfile}"
+        cd $gitdir && $gitcmd checkout -q -B upgradebranch origin/$upgrade
+        rm -fr config.php
+        ${phpcmd} admin/cli/install.php --non-interactive --allow-unstable --agree-license --wwwroot="http://localhost" --dataroot="$datadir" --dbtype=$dbtype --dbhost=$dbhost2 --dbname=$upgradedb --dbuser=$dbuser2 --dbpass=$dbpass2 --prefix=$dbprefixupgrade --fullname=$upgradedb --shortname=$upgradedb --adminuser=$dbuser2 --adminpass=$dbpass2 2>&1 >> "${logfile}"
+        # Error installing, we cannot continue. Exit
+        exitstatus=${PIPESTATUS[0]}
+        if [ $exitstatus -ne 0 ]; then
+            echo "Error: Problem installing Moodle $upgrade to test upgrade" | tee -a "${logfile}"
+        fi
+    fi
+
+    # Do the moodle upgrade
+    # only if we don't come from an erroneus previous situation
+    if [ $exitstatus -eq 0 ]; then
+        echo "Info: Upgrading Moodle $upgrade to $gitbranchinstalled into $upgradedb" | tee -a "${logfile}"
+        cd $gitdir && $gitcmd checkout -q installbranch
+        ${phpcmd} admin/cli/upgrade.php --non-interactive --allow-unstable 2>&1 >> "${logfile}"
+        # Error upgrading, inform and continue
+        exitstatus=${PIPESTATUS[0]}
+        if [ $exitstatus -ne 0 ]; then
+            echo "Error: Problem upgrading from $upgrade to $gitbranchinstalled" | tee -a "${logfile}"
+        fi
+    fi
+
+    # Run the DB compare utility, outputing problems if any.
+    # only if we don't come from an erroneus situation on upgrade
+    if [ $exitstatus -eq 0 ]; then
+        echo "Info: Comparing databases $installdb and $upgradedb" | tee -a "${logfile}"
+        ${phpcmd} $mydir/compare_databases.php --dblibrary=$dblibrary --dbtype=$dbtype --dbhost1=$dbhost1 --dbname1=$installdb --dbuser1=$dbuser1 --dbpass1=$dbpass1 --dbprefix1=$dbprefixinstall --dbhost2=$dbhost1 --dbname2=$upgradedb --dbuser2=$dbuser2 --dbpass2=$dbpass2 --dbprefix2=$dbprefixupgrade 2>&1 | tee -a "${logfile}"
+        exitstatus=${PIPESTATUS[0]}
+    fi
+
+    # Drop the upgraded database and delete files
+    # TODO: Based on $dbtype, execute different DB deletion commands
+    ${mysqlcmd} --user=${dbuser2} --password=${dbpass2} --host=${dbhost2} \
+        --execute="DROP DATABASE ${upgradedb}" 2>&1 >> "${logfile}"
     rm -fr config.php
-    ${phpcmd} admin/cli/install.php --non-interactive --allow-unstable --agree-license --wwwroot="http://localhost" --dataroot="$datadir" --dbtype=$dbtype --dbhost=$dbhost2 --dbname=$upgradedb --dbuser=$dbuser2 --dbpass=$dbpass2 --prefix=$dbprefixupgrade --fullname=$upgradedb --shortname=$upgradedb --adminuser=$dbuser2 --adminpass=$dbpass2
-    # Error installing, we cannot continue. Exit
-    exitstatus=${PIPESTATUS[0]}
+    rm -fr $datadir
+
+    # Feed the error counter
     if [ $exitstatus -ne 0 ]; then
-        echo "Error installing $gitbranchupgraded to test upgrade"
+        counterrors=$((counterrors+1))
+        echo "Error: Problem comparing databases $installdb and $upgradedb" | tee -a "${logfile}"
+    else
+        echo "Info: OK. No problems comparing databases $installdb and $upgradedb" | tee -a "${logfile}"
     fi
-fi
 
-# Do the moodle upgrade
-# only if we don't come from an erroneus previous situation
-if [ $exitstatus -eq 0 ]; then
-    # Need to checkout back to gitbranchinstalled from detached gitbranchupgraded
-    cd $gitdir && git checkout $gitbranchinstalled && git reset --hard $gitbranchinstalled
-    ${phpcmd} admin/cli/upgrade.php --non-interactive --allow-unstable
-    # Error upgrading, inform and continue
-    exitstatus=${PIPESTATUS[0]}
-    if [ $exitstatus -ne 0 ]; then
-        echo "Error upgrading from $gitbranchupgraded to $gitbranchinstalled"
-    fi
-fi
+done
 
-# Run the DB compare utility, saving results to file
-# only if we don't come from an erroneus situation on upgrade
-if [ $exitstatus -eq 0 ]; then
-    ${phpcmd} $mydir/compare_databases.php --dblibrary=$dblibrary --dbtype=$dbtype --dbhost1=$dbhost1 --dbname1=$installdb --dbuser1=$dbuser1 --dbpass1=$dbpass1 --dbprefix1=$dbprefixinstall --dbhost1=$dbhost1 --dbname2=$upgradedb --dbuser2=$dbuser2 --dbpass2=$dbpass2 --dbprefix2=$dbprefixupgrade > "$resultfile"
-    exitstatus=${PIPESTATUS[0]}
-fi
-
-# Drop the databases and delete files
+# Drop the installed databases and delete files
 # TODO: Based on $dbtype, execute different DB deletion commands
 ${mysqlcmd} --user=${dbuser1} --password=${dbpass1} --host=${dbhost1} \
-        --execute="DROP DATABASE ${installdb}"
-${mysqlcmd} --user=${dbuser2} --password=${dbpass2} --host=${dbhost2} \
-        --execute="DROP DATABASE ${upgradedb}"
+        --execute="DROP DATABASE ${installdb}" 2>&1 >> "${logfile}"
 rm -fr config.php
 rm -fr $datadir
+$gitcmd checkout -q $currentbranch
+$gitcmd branch -q -D installbranch upgradebranch
 
-# If arrived here, return the exitstatus of the php execution
-exit $exitstatus
+# If arrived here, return the counterrors of the php execution
+echo | tee -a "${logfile}"
+if [ $counterrors -ne 0 ]; then
+    echo "Error: Process ended with $counterrors errors" | tee -a "${logfile}"
+else
+    echo "Ok: Process ended without errors" | tee -a "${logfile}"
+fi
+exit $counterrors
