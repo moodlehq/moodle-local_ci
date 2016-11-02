af9d92af2fdc2872851bb815c6e9444ea418aca0
Dan Poltawski <dan@moodle.com>
2016-11-02 11:54:07 +0000

    MDLSITE-4787 mustache_lint: Handle theme templates better

--- mustache_lint/simple_core_component_mustache_loader.php
+++ mustache_lint/simple_core_component_mustache_loader.php
@@ -2,10 +2,12 @@
 
 class simple_core_component_mustache_loader extends Mustache_Loader_FilesystemLoader {
 
+    protected $theme = null;
     /**
-     * Provide a default no-args constructor (we don't really need anything).
+     * Constructor.
      */
-    public function __construct() {
+    public function __construct($theme = null) {
+        $this->theme = $theme;
     }
 
     /**
@@ -22,6 +24,17 @@ class simple_core_component_mustache_loader extends Mustache_Loader_FilesystemLo
         }
 
         list($component, $templatename) = explode('/', $name, 2);
+
+        if ($this->theme) {
+            // The real mustache loader handles theme parents - we don't here for simplicity.
+            $themedir = core_component::get_plugin_directory('theme', $this->theme);
+
+            $themetemplate = $themedir . '/templates/' . $component . '/'. $templatename . '.mustache';
+            if (file_exists($themetemplate)) {
+                return $themetemplate;
+            }
+        }
+
         $compdirectory = core_component::get_component_directory($component);
         $path = $compdirectory . '/templates/' . $templatename . '.mustache';
         if (!file_exists($path)) {
