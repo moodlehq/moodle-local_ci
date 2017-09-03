148a76984eeb66bd5a11debcbf5325dc64b4a1da
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2017-09-04 00:21:54 +0200

    MDLSITE-5184 grunt: cleanup to get rid of npmbase and extrapath
    Also, in order to be able to decide about the invokation of
    prepare_npm_stuff.sh, we add a new optional param, npminstall,

--- grunt_process/grunt_process.sh
+++ grunt_process/grunt_process.sh
@@ -2,19 +2,13 @@
 # $WORKSPACE: Directory where results are saved.
 # $gitdir: Directory containing git repo
 # $gitbranch: Branch we are going to install the DB
-# $extrapath: Extra paths to be available (global)
 # $npmcmd: Optional, path to the npm executable (global)
-# $npmbase: (optional) Base directory where we'll store multiple npm packages versions (subdirectories per branch)
+# $npminstall: (optional), if set the script will install nodejs stuff. Else, nodejs managing is external.
 
 # Let's be strict. Any problem leads to failure.
 set -e
 
-# Let add $extrapath to PATH
-if [[ -n ${extrapath} ]]; then
-    export PATH=${PATH}:${extrapath}
-fi
-
-required="WORKSPACE gitdir gitbranch extrapath"
+required="WORKSPACE gitdir gitbranch"
 for var in $required; do
     if [ -z "${!var}" ]; then
         echo "Error: ${var} environment variable is not defined. See the script comments."
@@ -37,21 +31,26 @@ rm -fr ${outputfile}
 rm -fr ${outputfile}.stderr
 
 # Prepare all the npm stuff if needed
-# (only if the job is in charge of handling it, aka, $npmbase was passed
-if [[ -n ${npmbase} ]]; then
+# (only if the job is in charge of handling it, aka, $npminstall was passed
+if [[ -n ${npminstall} ]]; then
     ${mydir}/../prepare_npm_stuff/prepare_npm_stuff.sh
 fi
 
-# Run grunt against the git repo
-cd ${gitdir}
-# First delete all build files so we can detect stale files in the build dir
-rm -fr $(find . -path '*/yui/build' -type d)
-rm -fr $(find . -path '*/amd/build' -type d)
-
-# Send both stdout and stderr to files while passing them intact (for callers consumption).
-# The echo here works around a problem where shifter is sending colours (MDL-52591).
+# Ensure we have grunt cli available before continue.
 gruntcmd="$(${npmcmd} bin)"/grunt
-if [ -x $gruntcmd ]; then
+if [ ! -x $gruntcmd ]; then
+    echo "Error: grunt executable not found" | tee "${outputfile}"
+    exitstatus=1
+else
+    # Run grunt against the git repo
+    cd ${gitdir}
+
+    # First delete all build files so we can detect stale files in the build dir
+    rm -fr $(find . -path '*/yui/build' -type d)
+    rm -fr $(find . -path '*/amd/build' -type d)
+
+    # Send both stdout and stderr to files while passing them intact (for callers consumption).
+    # The echo here works around a problem where shifter is sending colours (MDL-52591).
     # Run the default task (same as specifying no arguments)
     tasks="default"
 
@@ -65,15 +64,12 @@ if [ -x $gruntcmd ]; then
     $gruntcmd $tasks --no-color > >(tee "${outputfile}") 2> >(tee "${outputfile}".stderr >&2)
     exitstatus=$?
     set -e
-else
-    echo "Error: grunt executable not found" | tee "${outputfile}"
-    exit 1
 fi
 
-# Cleanup symlink as not required after run (and prevent other jobs operating on it)
-# (only if the job is in charge of handling it, aka, $npmbase was passed
-if [[ -n ${npmbase} ]]; then
-    rm ${gitdir}/node_modules
+# Cleanup the nodejs installed stuff, not required after run (and prevent other jobs operating on it)
+# (only if the job is in charge of handling it, aka, $npminstall was passed
+if [[ -n ${npminstall} ]]; then
+    rm -fr ${gitdir}/node_modules
 fi
 
 if [ $exitstatus -ne 0 ]; then
