a5a8fc081798690680cefe679e8fe9eb6fc56711
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2021-09-13 10:30:17 +0200

    Add support to specify the runner to use
    Defaulting to STABLE, also TESTING can be used.
    
    Only to the manual "list_of_mdls" jobs (normal and security).
    
    Also, detected some stuff pending in the security one, about
    the ability to pick the jobs to execute, so reconciling it
    to be feature-paired with the normal one.

--- tracker_automations/bulk_prelaunch_jobs/criteria/list_of_mdls_sdev/jobs.sh
+++ tracker_automations/bulk_prelaunch_jobs/criteria/list_of_mdls_sdev/jobs.sh
@@ -5,17 +5,24 @@
 # to stop silently. This was discovered @ MDLSITE-5313
 # and we need to keep it (until we move to REST from CLI)
 
+# Set the runner if not specified.
+runner="${runner:-STABLE}"
+
 # We want to launch always a sqlsrv PHPUNIT
-echo -n "PHPUnit (sqlsrv): " >> "${resultfile}.jenkinscli"
-${jenkinsreq} "SDEV.02 - Developer-requested PHPUnit" \
-    -p REPOSITORY=${repository} \
-    -p BRANCH=${branch} \
-    -p DATABASE=sqlsrv \
-    -p PHPVERSION=${php_version} \
-    -w >> "${resultfile}.jenkinscli" < /dev/null
+if [[ "${jobtype}" == "all" ]] || [[ "${jobtype}" == "phpunit" ]]; then
+    echo -n "PHPUnit (sqlsrv): " >> "${resultfile}.jenkinscli"
+    ${jenkinsreq} "SDEV.02 - Developer-requested PHPUnit" \
+        -p REPOSITORY=${repository} \
+        -p BRANCH=${branch} \
+        -p DATABASE=sqlsrv \
+        -p PHPVERSION=${php_version} \
+        -p RUNNERVERSION=${runner} \
+        -w >> "${resultfile}.jenkinscli" < /dev/null
+fi
 
 # Disabled for now, it's failing a lot :-(
 # We want to launch always a Behat (latest, @app only) job
+#if [[ "${jobtype}" == "all" ]] || [[ "${jobtype}" == "behat-app" ]]; then
 #echo -n "App tests (experimental): " >> "${resultfile}.jenkinscli"
 #${jenkinsreq} "SDEV.01 - Developer-requested Behat" \
 #    -p REPOSITORY=${repository} \
@@ -27,24 +34,32 @@ ${jenkinsreq} "SDEV.02 - Developer-requested PHPUnit" \
 #    -p MOBILE_VERSION=latest \
 #    -p INSTALL_PLUGINAPP=true \
 #    -p TAGS=@app \
+#    -p RUNNERVERSION=${runner} \
 #    -w >> "${resultfile}.jenkinscli" < /dev/null
+#fi
 
 # We want to launch always a Behat (goutte) job
-echo -n "Behat (goutte): " >> "${resultfile}.jenkinscli"
-${jenkinsreq} "SDEV.01 - Developer-requested Behat" \
-    -p REPOSITORY=${repository} \
-    -p BRANCH=${branch} \
-    -p DATABASE=pgsql \
-    -p PHPVERSION=${php_version} \
-    -p BROWSER=goutte \
-    -w >> "${resultfile}.jenkinscli" < /dev/null
+if [[ "${jobtype}" == "all" ]] || [[ "${jobtype}" == "behat-all" ]] || [[ "${jobtype}" == "behat-goutte" ]]; then
+    echo -n "Behat (goutte): " >> "${resultfile}.jenkinscli"
+    ${jenkinsreq} "SDEV.01 - Developer-requested Behat" \
+        -p REPOSITORY=${repository} \
+        -p BRANCH=${branch} \
+        -p DATABASE=pgsql \
+        -p PHPVERSION=${php_version} \
+        -p BROWSER=goutte \
+        -p RUNNERVERSION=${runner} \
+        -w >> "${resultfile}.jenkinscli" < /dev/null
+fi
 
 # We want to launch always a Behat (firefox) job
-echo -n "Behat (firefox): " >> "${resultfile}.jenkinscli"
-${jenkinsreq} "SDEV.01 - Developer-requested Behat" \
-    -p REPOSITORY=${repository} \
-    -p BRANCH=${branch} \
-    -p DATABASE=pgsql \
-    -p PHPVERSION=${php_version} \
-    -p BROWSER=firefox \
-    -w >> "${resultfile}.jenkinscli" < /dev/null
+if [[ "${jobtype}" == "all" ]] || [[ "${jobtype}" == "behat-all" ]] || [[ "${jobtype}" == "behat-firefox" ]]; then
+    echo -n "Behat (firefox): " >> "${resultfile}.jenkinscli"
+    ${jenkinsreq} "SDEV.01 - Developer-requested Behat" \
+        -p REPOSITORY=${repository} \
+        -p BRANCH=${branch} \
+        -p DATABASE=pgsql \
+        -p PHPVERSION=${php_version} \
+        -p BROWSER=firefox \
+        -p RUNNERVERSION=${runner} \
+        -w >> "${resultfile}.jenkinscli" < /dev/null
+fi
