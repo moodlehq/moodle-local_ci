dc590f5bc3b918d503d2d3aabfb850d55da4bad6
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2021-09-23 23:44:41 +0200

    Lower the priority of blocked issues
    This changes are very similar to the ones used to
    raise priority of blocker issues but, instead, lowers
    the priority of any issue that has unresolved blockers.
    
    Why? Because the blockers always need to be processed earlier
    (and will get the priority raised), hence the "blocked" ones
    better get the priority lowered.
    
    I've run the 2 scripts a number of times (raise & lower) and
    no ping-pong effect is possible within them, those raised remain
    raised and those lowered remain lowered.
    
    (unless something changes in the issues, of course, say because
    some of the blockers are resolved or whatever)
    
    After this, there will be another commit to make to the queues
    managers also aware of the blocking status, so they won't be
    undoing (raising) the priority of issues lowered by this job.
    
    Also note that a few small changes have been applied to the
    sister set_integration_priority_to_one.sh script to better
    inform in console about the decisions taken. Maybe we'll end
    also adding a restricted comment in the issue to know which
    script was in charge of deciding the change of priority (but
    it's not strictly needed now).

--- tracker_automations/set_integration_priority_to_zero/set_integration_priority_to_zero.sh
+++ tracker_automations/set_integration_priority_to_zero/set_integration_priority_to_zero.sh
@@ -57,5 +57,72 @@ for issue in $( sed -n 's/^"\(MDL-[0-9]*\)".*/\1/p' "${resultfile}" ); do
     echo "$BUILD_NUMBER $BUILD_TIMESTAMP ${issue}" >> "${logfile}"
 done
 
+# Now, let's look for all the issues, also having > 0 integration priority and
+# being under current integration or awaiting integration. Lower the integration
+# priority for those being blocked by unresolved issues.
+#
+# Note that this can be achieved using a simple query using features provided by the J-Tricks
+# plugin (and others), but they won't be compatible with Jira Cloud instances, so we are doing
+# it using exclusively JiraCLI and its facilities (requiring multiple actions to be executed).
+
+# First, get all the issues that are blocked by others.
+${basereq} --action getIssueList \
+           --search "project = 'Moodle' \
+                 AND 'Integration priority' > 0 \
+                 AND ( \
+                       ('Currently in integration' = 'Yes' AND status != 'Reopened') \
+                       OR status = 'Waiting for integration review' \
+                     ) \
+                 AND issueLinkType = 'is blocked by'" \
+           --file "${resultfile}"
+
+# Iterate over found issues and get its list of links being "is blocked by"
+for issue in $( sed -n 's/^"\(MDL-[0-9]*\)".*/\1/p' "${resultfile}" ); do
+    echo "Processing ${issue}"
+    ${basereq} --action getLinkList \
+               --issue "${issue}" \
+               --columns "To Issue" \
+               --regex "(?i)is blocked by" \
+               --file "${resultfile}"
+
+    # Iterate over found "is blocked by" issues and concat them for the next query.
+    blockedbyissues=
+    for linkedissue in $( sed -n 's/^"\(MDL-[0-9]*\)".*/\1/p' "${resultfile}" ); do
+        blockedbyissues="${blockedbyissues} ${linkedissue},"
+    done
+    blockedbyissues=${blockedbyissues%?}
+
+    # Now let's see if any of the blockedby issues is unresolved.
+    # (note that, since JiraCLU 8.1, getIssueCount can be used instead, but we are using older)
+    if [[ -n ${blockedbyissues} ]]; then
+        ${basereq} --action getIssueList \
+                   --search "resolution = Unresolved AND issue IN (${blockedbyissues})" \
+                   --file "${resultfile}"
+        # If there are issues returned... then the issue still has unresolved blockers.
+        unresolvedfound=
+        for unresolvedissue in $( sed -n 's/^"\(MDL-[0-9]*\)".*/\1/p' "${resultfile}" ); do
+            echo "  ${unresolvedissue} blocks it and it is unresolved."
+            unresolvedfound=1
+        done
+
+        # If there aren't unresolved blockers, skip this issue.
+        if [[ -z ${unresolvedfound} ]]; then
+            echo "  skipping this issue (all blockers are resolved)"
+            echo
+            continue
+        fi
+    fi
+
+    # Arrived here, this is an issue that is blocked by some unresolved issue.
+    # So we lower  its priority here and now.
+    echo "  lowering its integration priority to 0 (has unresolved blockers)"
+    ${basereq} --action progressIssue \
+        --issue ${issue} \
+        --step "CI Global Self-Transition" \
+        --custom "customfield_12210:0"
+        echo "$BUILD_NUMBER $BUILD_TIMESTAMP ${issue}" >> "${logfile}"
+    echo
+done
+
 # Remove the resultfile. We don't want to disclose those details.
 rm -fr "${resultfile}"
