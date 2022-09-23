74f95b0f5c1cd57942ebcc3be9993ee3af814e39
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2022-09-23 09:16:57 +0200

    More "cime" label case-sensitive problems @ MDL-64431
    Pity search is case-insensitive and CRUD are sensitive. Only
    possible solution is to create some job that fetches all issues
    having those labels (in a insensitive way), and then go iterating
    over all them, updating the wrongly cased ones.

--- tracker_automations/bulk_precheck_issues/criteria/developer_request/postissue.sh
+++ tracker_automations/bulk_precheck_issues/criteria/developer_request/postissue.sh
@@ -1,7 +1,7 @@
 # Remove the "cime" label.
 ${basereq} --action removeLabels \
     --issue ${issue} \
-    --labels "cime CIME" # the uppercase to fix MDLSITE-4716
+    --labels "cime CIME Cime CiMe" # the uppercase to fix MDLSITE-4716 and cases like MDL-64431
 
 # Add the comment with results
 comment=$(cat "${resultfile}.${issue}.txt")
