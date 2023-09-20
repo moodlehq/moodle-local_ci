8b0764adb392f108c6d77d0ffd063bc303c66d3e
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2023-09-20 12:03:50 +0200

    First cut to the CLR/IR automated triaging
    Note that this is not finished yet, but it already
    is able to send a good number of issues to IR:
    - Issues without component.
    - Component not in the sheet.
    - Component without lead group assigned.
    - Conflict between components group leads.
    - Component doesn't have any reviewer available.
    - Conflict between available reviewers.
    
    There are still some more verifications and actions
    to add to the script, but this will allow us to verify
    that everything (configuration, web-services, permissions...)
    is working ok, while also start triaging some obvious
    issues to the IR outcome, reducing the list of issues to
    manually triage.

---
+++ tracker_automations/component_leads_integration_mover/component_leads_integration_mover.sh
@@ -0,0 +1,156 @@
+#!/usr/bin/env bash
+# Looks for all undecided issues awaiting integration and decides about sending them
+# to Component Leads Review (clr) or Integration Review (ir).
+#jiraclicmd: fill execution path of the jira cli
+#jiraserver: jira server url we are going to connect to
+#jirauser: user that will perform the execution
+#jirapass: password of the user
+#jsonclrurl: url to the webservice providing all the groups, components and reviewers data.
+#quiet: if enabled ("true"), don't perform any action in the Tracker.
+#restrictedto: if set, restrict any comment to that role in the project. Blank means visible to everybody.
+
+# Let's go strict (exit on error)
+set -e
+
+# Verify everything is set
+required="WORKSPACE jiraclicmd jiraserver jirauser jirapass jsonclrurl"
+for var in $required; do
+    if [ -z "${!var}" ]; then
+        echo "Error: ${var} environment variable is not defined. See the script comments."
+        exit 1
+    fi
+done
+
+# We need curl to execute this script.
+if [[ ! $(which curl) ]]; then
+    echo "Error: This script needs \"curl\" installed to work"
+    exit 1
+fi
+
+# We need jq to execute this script.
+if [[ ! $(which jq) ]]; then
+    echo "Error: This script needs \"jq\" installed to work"
+    exit 1
+fi
+
+# file where results will be sent
+resultfile=${WORKSPACE}/component_leads_integration_mover.json
+echo -n > "${resultfile}"
+
+# file where actions peformed will be logged
+logfile=${WORKSPACE}/component_leads_integration_mover.log
+
+# file where the components, groups and CLRs will be stored.
+clrfile=${WORKSPACE}/clr.json
+
+# Calculate some variables
+mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+basereq="${jiraclicmd} --server ${jiraserver} --user ${jirauser} --password ${jirapass}"
+
+# If we don't have the clr.json information at hand, let's download it.
+if [[ ! -r "${clrfile}" ]]; then
+    echo "Downloading the CLR metadadata information"
+    curl -s -o ${clrfile} $jsonclrurl
+fi
+
+# If existing clr.json file is older than 48h, let's download it.
+if [[ $(find ${clrfile} -mmin +$((48*60)) -print | grep clr.json) ]]; then
+    echo "Updating the CLR metadadata information."
+    if [[ $(curl -s -o ${clrfile}.tmp $jsonclrurl) ]]; then
+        mv ${clrfile}.tmp ${clrfile}
+    else
+        echo "Problem downloading the CLR metadadata information."
+        # Touch cached one, so next run will work.
+        touch  ${clrfile}
+        echo "Using existing cached metadata file for next (48h) runs. You can now execute this again."
+        echo "Please, verify the causes of the download problem."
+        exit 1
+    fi
+fi
+
+source ${mydir}/lib.sh # Add all the functions.
+
+# Search for all the issues awaiting for integration and not being decided between CLR/IR.
+# Note: customfield_10118 is the peer reviewer custom field.
+${basereq} --action getIssueList \
+           --jql "filter = 23535" \
+           --columns="Key,Assignee,Peer reviewer,Components" \
+           --outputFormat=2 \
+           --outputType=json \
+           --file "${resultfile}"
+
+# If there aren't issues, we have finished.
+if [[ ! $(grep '"components":' "${resultfile}") ]]; then
+    echo "No issues to process."
+    exit 0
+fi
+
+# Iterate over found issues and perform the actions with them.
+jq -c '.[]' ${resultfile} | while read json; do
+    # Get the issue
+    issue=$(jq -r '.key' <<< $json)
+    echo "Processing ${issue}"
+    # Get assignee, peer reviewer and components.
+    assignee=$(jq -r '.assignee' <<< $json)
+    peerreviewer=$(jq -r '.peerReviewer' <<< $json)
+    components=$(jq -r '.components' <<< $json)
+
+    # Reset the outcome (defaults to no action and no description).
+    outcome=
+    outcomedesc=
+
+    # Let's calculate the CLR/IR outcome.
+    triage_issue
+
+    # Arrived here, if we have an outcome, we are going to set the "Component Lead Review" field, only if $quiet is false.
+    if [[ ${quiet} == "false" ]] && [[ -n ${outcome} ]]; then
+        # Let's see if there is any restriction to the comment in the Tracker
+        restrictiontype=
+        if [[ -n "${restrictedto}" ]]; then
+            restrictiontype=--role
+        fi
+        echo "  - Sending results to the Tracker (${restrictiontype} ${restrictedto})"
+
+        # For fields available in the default screen, it's ok to use updateIssue or SetField, but in this case
+        # we are setting some custom fields not available (on purpose) on that screen. So we have created a
+        # global transition, only available to the bots, not transitioning but bringing access to all the fields
+        # via special screen. So we'll ne using that global transition via transitionIssue instead.
+        # customfield_15810 is the "Component Lead Review" field (Yes => CLR, No => IR, empty => undecided).
+        if [[ "${outcome}" == "IR" ]]; then
+            # No CLR. Just update the field.
+            ${basereq} --action transitionIssue \
+                       --issue ${issue} \
+                       --transition "CI Global Self-Transition" \
+                       --field "customfield_15810=No"
+        else
+            # CLR. Real transition to Waiting for CLR.
+            # Comment not restricted.
+            ${basereq} --action transitionIssue \
+                       --issue ${issue} \
+                       --transition "Send to Component Leads Review"
+        fi
+
+        # Now, if there is some outcome description, add it as a comment.
+        if [[ -n ${outcomedesc} ]]; then
+            if [[ -n "${restrictedto}" ]]; then
+                # Comment restricted.
+                ${basereq} --action addComment \
+                           --issue ${issue} \
+                           --comment "${outcomedesc}" \
+                           ${restrictiontype} "${restrictedto}"
+            else
+                # Comment not restricted.
+                ${basereq} --action addComment \
+                           --issue ${issue} \
+                           --comment "${outcomedesc}"
+            fi
+        fi
+    else
+        # This is a quiet run, just output the outcome.
+        echo "  - Outcome: $outcome"
+        echo "  - Comment: $outcomedesc"
+    fi
+done
+
+# Remove the resultfile. We don't want to disclose those details.
+rm -fr "${resultfile}"
---
+++ tracker_automations/component_leads_integration_mover/lib.sh
@@ -0,0 +1,187 @@
+#!/usr/bin/env bash
+# Functions to triage issues between CLR/IR (used by component_leads_integration_mover.sh)
+
+# Let's go strict (exit on error)
+set -e
+
+# Main function to apply for all the rules agreed and decide between CLR/IR.
+# Note that, for now, none of the criteria include looking to code, but
+# just makes a decision based on tracker information 100%. No matter of that,
+# it's possible to create new criteria performing code verifications or
+# anything else.
+function triage_issue() {
+    echo "  Issue: $issue, Assignee: $assignee, Peer reviewer: $peerreviewer, Components: $components"
+
+    # Verify that all the components are valid. Send to integration if not.
+    verify_components_are_valid && [[ -n $outcome ]] && return
+
+    # Verify that all the components belong to the same group. Send to integration if not.
+    verify_components_same_group && [[ -n $outcome ]] && return
+
+    # Verify reviewers availability, checking the assignee and peer reviewers against the list of component reviewers.
+    verify_revievers_availability && [[ -n $outcome ]] && return
+
+    # To continue... adding more rules until we have all the devpad annotated ones covered.
+}
+
+function verify_components_are_valid {
+    echo "  - verify_components_are_valid"
+
+    # Trim it, just in case.
+    components=$(trimstring "$components")
+    # Verify that we have components
+    if [[ ${#components} -eq 0 ]]; then
+        echo "    - Problem: No components"
+        outcome=IR
+        outcomedesc="Sending to IR, the issue is missing components so it cannot be decided."
+        return # Outcome set, we are done.
+    fi
+
+    # Verify that all the components are in the sheet
+    IFS=, read -r -a componentsArr <<<"$components"
+    for component in "${componentsArr[@]}"; do
+        component=$(trimstring "$component")
+        echo "    - component: $component"
+        if [[ ! $(jq -e ".trackerComponents[] | select(.component == \"${component}\")" ${clrfile}) ]]; then
+            echo "      - Problem: Component is not in the sheet."
+            outcome=IR
+            outcomedesc="Sending to IR, the \"${component}\" component is not in the sheet."
+            return # Outcome set, we are done.
+        fi
+    done
+}
+
+function verify_components_same_group {
+    echo "  - verify_components_same_group"
+
+    # Trim it, just in case.
+    components=$(trimstring "$components")
+
+    # Verify that all the components belong to the same group
+    leadgroup=
+    IFS=, read -r -a componentsArr <<<"$components"
+    for component in "${componentsArr[@]}"; do
+        component=$(trimstring "$component")
+        echo "    - component: $component"
+        group=$(get_lead_component_group "${component}")
+        if [[ -z ${group} ]]; then
+            echo "      - Problem: Component \"${component}\" does not have a lead team assigned."
+            outcome=IR
+            outcomedesc="Sending to IR, the \"${component}\" component does not have a lead team assigned."
+            return # Outcome set, we are done.
+        fi
+        if [[ -n ${leadgroup} ]] && [[ ${leadgroup} != ${group} ]]; then
+            echo "      - Problem: Multiple lead groups detected: \"${leadgroup}\" and  \"${group}\"."
+            outcome=IR
+            outcomedesc="Sending to IR, multiple lead groups detected: \"${leadgroup}\" and \"${group}\"."
+            return # Outcome set, we are done.
+        fi
+        leadgroup=${group}
+    done
+}
+
+function verify_revievers_availability() {
+    echo "  - verify_revievers_availability"
+
+    # Trim it, just in case.
+    components=$(trimstring "$components")
+
+    # First, look if the components have some, specific or group, reviewers.
+    leadreviewers=
+    IFS=, read -r -a componentsArr <<<"$components"
+    for component in "${componentsArr[@]}"; do
+        reviewers=
+        component=$(trimstring "$component")
+        echo "    - component: $component"
+        reviewers=$(get_lead_component_reviewers "${component}")
+        reviewers=$(trimstring "$reviewers")
+        reviewers=$(echo -n "${reviewers}" | tr -s '[:space:]' ',') # Cannot use <<<, it adds a \n.
+        if [[ -z ${reviewers} ]]; then
+            echo "      - Problem:: Component \"${component}\" does not have any, specific or group, reviewer available."
+            outcome=IR
+            outcomedesc="Sending to IR, component \"${component}\" does not have any, specific or group, reviewer available."
+            return # Outcome set, we are done.
+        elif [[ -n ${leadreviewers} ]] && [[ ${leadreviewers} != ${reviewers} ]]; then
+            echo "      - Problem: Conflicting component reviewers detected: \"${leadreviewers}\" and \"${reviewers}\"."
+            outcome=IR
+            outcomedesc="Sending to IR, conflicting component reviewers detected: \"${leadreviewers}\" and \"${reviewers}\"."
+            return # Outcome set, we are done.
+        fi
+        leadreviewers=${reviewers}
+    done
+    echo "      - Reviewers: ${leadreviewers}"
+
+    # We have some reviewers, verify that they are available and haven't played
+    # any role in the issue (assignee or peer-reviewer)
+
+}
+
+function another {
+    echo "  - another"
+}
+
+function trimstring() {
+    if [ $# -ne 1 ]
+    then
+        echo "USAGE: trimstring [STRING]"
+        return 1
+    fi
+    s="${1}"
+    size_before=${#s}
+    size_after=0
+    while [ ${size_before} -ne ${size_after} ]
+    do
+        size_before=${#s}
+        s="${s#[[:space:]]}"
+        s="${s%[[:space:]]}"
+        size_after=${#s}
+    done
+    echo "${s}"
+    return 0
+}
+
+# Given a component, return the group that leads it
+function get_lead_component_group() {
+    if [ $# -ne 1 ]
+    then
+        echo "USAGE: get_lead_component_group [COMPONENT]"
+        return 1
+    fi
+    local component="${1}"
+    local group=
+
+    group=$(jq -r ".trackerComponents[] | select(.component == \"${component}\") | .group" ${clrfile})
+    group=$(trimstring "$group")
+    echo "${group}"
+    return
+}
+
+# Given a component, return the list of reviewers available for it.
+# First look for component specific reviewer, then for group reviewers.
+function get_lead_component_reviewers() {
+    if [ $# -ne 1 ]
+    then
+        echo "USAGE: get_lead_component_reviewers [COMPONENT]"
+        return 1
+    fi
+    local component="${1}"
+    local reviewers=
+
+    # First, look for component specific reviewers.
+    reviewers=$(jq -r ".reviewersAvailability[] | select (.component == \"${component}\") .reviewers[]" ${clrfile})
+    reviewers=$(trimstring "$reviewers")
+    if [[ -n ${reviewers} ]]; then
+        echo "${reviewers}"
+        return
+    fi
+
+    # No specific reviewers found, let's look for group ones.
+    # Let's find the leading group of the component.
+    local group=$(get_lead_component_group "${component}")
+    if [[ -n ${group} ]]; then
+        reviewers=$(jq -r ".reviewersAvailability[] | select (.group == \"${group}\") .reviewers[]" ${clrfile})
+        reviewers=$(trimstring "$reviewers")
+        echo "${reviewers}"
+        return
+    fi
+}
