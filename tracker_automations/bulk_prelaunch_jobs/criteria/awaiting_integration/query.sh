0141c29c189454b0a41bb993f9f4fd348943206d
Simey Lameze <simey@moodle.com>
2025-12-16 17:38:04 +0800

    Add support for CLR fields

--- tracker_automations/bulk_prelaunch_jobs/criteria/awaiting_integration/query.sh
+++ tracker_automations/bulk_prelaunch_jobs/criteria/awaiting_integration/query.sh
@@ -1,7 +1,7 @@
 ${basereq} --action getIssueList \
            --jql "project = 'Moodle' \
-                 AND status = 'Waiting for integration review' \
-                 AND status WAS NOT 'Waiting for integration review' ON '-${schedulemins}' \
+                 AND status IN ('Waiting for integration review', 'Waiting for component lead review')  \
+                 AND status WAS NOT IN ('Waiting for integration review', 'Waiting for component lead review') ON '-${schedulemins}' \
                  AND level IS EMPTY \
                  AND 'Automated test results' IS EMPTY \
                  ORDER BY priority DESC, votes DESC, 'Last comment date' ASC" \
