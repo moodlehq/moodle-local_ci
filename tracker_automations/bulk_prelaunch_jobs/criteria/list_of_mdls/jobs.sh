bc827689cbfa3de1c355ff440fe8a87bac2eb951
Simey Lameze <simey@moodle.com>
2026-04-21 16:05:37 +0800

    Use SELVERSION parameter

--- tracker_automations/bulk_prelaunch_jobs/criteria/list_of_mdls/jobs.sh
+++ tracker_automations/bulk_prelaunch_jobs/criteria/list_of_mdls/jobs.sh
@@ -180,8 +180,7 @@ if [[ "${jobtype}" == "all" ]] || [[ "${jobtype}" == "behat-all" ]] || [[ "${job
             -p DATABASE=pgsql \
             -p PHPVERSION=${php_version} \
             -p BROWSER="Chrome (js)" \
-            -p USE_SELVERSION=true \
-            -p SELVERSION='145.0-20260222' \
+            -p USE_SELVERSION='145.0-20260222' \
             -p BEHAT_INCREASE_TIMEOUT=4 \
             -p MOBILE_VERSION=latest-test \
             -p INSTALL_PLUGINAPP=ci \
